cedef49ab6b3681b68e3c0970c663ee780ae49dd
barani0709 <baranidharan@elbrit.org>
2025-06-16 14:40:13 +0530

    latest update

--- src/auth/msalInstance.js
+++ src/auth/msalInstance.js
@@ -4,7 +4,7 @@ export const msalInstance = new PublicClientApplication({
   auth: {
     clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
     authority: `https://login.microsoftonline.com/${process.env.NEXT_PUBLIC_TENANT_ID}`,
-    redirectUri: process.env.NEXT_PUBLIC_REDIRECT_URI || "http://localhost:3000",
+    redirectUri: process.env.NEXT_PUBLIC_REDIRECT_URI || "https://elbirt-dev.netlify.app/",
   },
   cache: {
     cacheLocation: "localStorage", // <-- this persists even after refresh!
